3546214beb9fdaeb48fbaa5bbbc9a590b9ca8ae9
ezsiddharth <145989806+ezsiddharth@users.noreply.github.com>
2023-10-04 19:31:58 +0530

    Patient Edit Updated

--- DCRM_UI/DCRMUI/src/app/main/clinic-admin/patient/patient-edit/patient-edit.component.ts
+++ DCRM_UI/DCRMUI/src/app/main/clinic-admin/patient/patient-edit/patient-edit.component.ts
@@ -1,86 +1,221 @@
-﻿// edit-patient.component.ts
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+﻿import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { repeaterAnimation } from 'app/main/apps/invoice/invoice.animation';
+import { PatientEditService } from 'app/main/clinic-admin/patient/patient-edit/patient-edit.service';
+import { PatientAddModel } from '../patient-add/patient-add.model';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { PatientEditService } from './patient-edit.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-edit-patient',
-  templateUrl: './patient-edit.component.html',
-  styleUrls: ['./patient-edit.component.scss'],
+    selector: 'app-patient-edit',
+    templateUrl: './patient-edit.component.html',
+    styleUrls: ['./patient-edit.component.scss'],
+    animations: [repeaterAnimation],
+    encapsulation: ViewEncapsulation.None
 })
-export class PatientEditComponent implements OnInit {
-  patientId: number;
-  patientData: any;
+export class PatientEditComponent implements OnInit, OnDestroy {
   editPatientForm: FormGroup;
+    // Public
+    public url = this.router.url;
+    public urlLastValue;
+    public apiData;
+    public sidebarToggleRef = false;
+    public paymentSidebarToggle = false;
+    public addPatientForm: UntypedFormGroup;
+    public items = [{ itemType: '', itemName: '', itemAmount: '', itemSpent: '', itemBalance: ''}];
+    public loading = false;
+    public submitted = false;
+    public returnUrl: string;
+    public error = '';
+    imagePreviewUrl: string | ArrayBuffer | null = null;
+    public patient: PatientAddModel = {
+      title: '',
+      photo: null,
+      name: '',
+      guardian: '',
+      sex: '',
+      dob: null,
+      age: null,
+      weight: null,
+      mobile: null,
+      present_address: '',
+      permanent_address: '',
+      patientContacts: [{
+        phone1: null,
+        phone2: null,
+        phone3: null,
+        phone4: null,
+        email: '',
+        email2: '',
+        address_R: '',
+        city_R: '',
+        zip_R: '',
+        country_R: '',
+        address_O: '',
+        city_O: '',
+        zip_O: '',
+        country_O: '',
+        address_Other: '',
+        city_Other: '',
+        zip_Other: null,
+        country_Other: '',
+        physician: '',
+        reffered_By: '',
+        doctor_Name: '',
+        phone: null,
+        relationship_Type: '',
+        history_Allergies: '',
+        special_Notes: '',  
+      }],
+      insurance_loan: [],
+    }
+    // Private
+    private _unsubscribeAll: Subject<any>;
+    //private _formBuilder: any;
 
-  constructor(
-    private route: ActivatedRoute,
-    private patientEditService: PatientEditService,
-    private fb: FormBuilder
-  ) {}
+    /**
+     * Constructor
+     *
+     * @param {Router} router
+     * @param {InvoiceEditService} _invoiceEditService
+     * @param {CoreSidebarService} _coreSidebarService
+     */
+    constructor(
+        private router: Router,
+        private _patientEditService: PatientEditService, private _formBuilder: UntypedFormBuilder, private _route: ActivatedRoute, private _toastrService: ToastrService) {
+        this._unsubscribeAll = new Subject();
+    }
+    addNewItem() {
+        this.items.push({
+            itemType: '',
+            itemName: '',
+            itemAmount: '',
+            itemBalance: '',
+            itemSpent: '',
+        });
+    }
+    deleteItem(id) {
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items.indexOf(this.items[i]) === id) {
+                this.items.splice(i, 1);
+                break;
+            }
+        }
+    }
 
-  imagePreviewUrl: string = ''; 
+    
+    base64Image: string = "";
+    selectedImage: File | null = null;
 
-  // Define the convertToBase64 method
-  convertToBase64(event: any) {
-    // Logic for converting the selected image to base64 goes here
-  }
+    // Define the convertToBase64 method
+    convertToBase64(event: any) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.base64Image = e.target.result;
+        };
+        reader.onload = (e: any) => {
+          this.imagePreviewUrl = e.target.result;
+        };
+        reader.readAsDataURL(file);
+        reader.readAsDataURL(this.selectedImage);
+      }
+    }
 
-  // Define your form group and other properties here
-  base64Image: string = ''; 
+    /**
+     * On init
+     */
+    ngOnInit(): void {
 
-  items: any[] = []; 
+        // this._patientEditService.onMedicinBrandChanged.subscribe(res => (this.medicinBrands = res));
+        // this._patientEditService.onMedicinCategoriesChanged.subscribe(res => (this.medicinCategories = res));
+        this.addPatientForm = this._formBuilder.group({
+          title: ['select'],
+          name: ['', Validators.required],
+          guardian: [''],
+          sex: [''],
+          dob: [null],
+          age: [null],
+          weight: [null],
+          mobile: [null],
+          present_address: [''],
+          permanent_address: [''],
+          patientContacts: {
+            phone1: [null, Validators.required],
+            phone2: [null],
+            phone3: [null],
+            phone4: [null],
+            email: [''],
+            email2: [''],
+            address_R: [''],
+            city_R: [''],
+            zip_R: [''],
+            country_R: [''],
+            address_O: [''],
+            city_O: [''],
+            zip_O: [''],
+            country_O: [''],
+            address_Other: [''],
+            city_Other: [''],
+            zip_Other: [''],
+            country_Other: [''],
+            physician: [''],
+            referred_by: [''],
+            doctor_name: [''],
+            phone_doctor: [null],
+            relationship_type: [''],
+            history_allergies: [''],
+            special_Notes: [''],
+          }
+        });
+        this._patientEditService.onPatientEditChanged.pipe(takeUntil(this._unsubscribeAll)).subscribe(response => {
+             this.patient = response;
+        });
+    }
+    get f() {
+      return this.addPatientForm.controls;
+    }
+    onCategorySelected(ob) {
 
-  // Define the error property
-  error: string | null = null;
+    }
+    onBrandSelected(ob) {
 
-  // Define the addNewItem method
-  addNewItem() {
-    this.items.push({
-        type: '',
-        name: '',
-        amount: null,
-        balance_spent: null,
-        balance_amount: null
-    });
-  }
-
-  removeItem(index: number) {
-    this.items.splice(index, 1);
-  }
-
-  // Define the saveData method
-  saveData() {
-    // Logic for saving data goes here
-  }
-
-  ngOnInit(): void {
-    this.patientId = +this.route.snapshot.paramMap.get('id');
-    this.loadPatientData();
-    this.createForm();
-  }
-
-  loadPatientData(): void {
-    this.patientEditService.getPatient(this.patientId).subscribe((data) => {
-      this.patientData = data;
-      this.editPatientForm.patchValue(this.patientData); // Populate the form with patient data
-    });
-  }
-
-  createForm(): void {
-    this.editPatientForm = this.fb.group({
-      // Define form controls corresponding to patient data fields
-      // For example: name, gender, date of birth, etc.
-    });
-  }
-
-  onSubmit(): void {
-    const updatedPatientData = this.editPatientForm.value;
-    this.patientEditService
-      .updatePatient(this.patientId, updatedPatientData)
-      .subscribe((response) => {
-        // Handle the response as needed
-      });
-  }
+    }
+    cancel() {
+        this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/admin/patient/list';
+        this.router.navigateByUrl(this.returnUrl);
+    }
+    onSubmit() {
+        this.submitted = true;
+       
+        if (this.addPatientForm.invalid) {
+            return;
+        }
+       
+        this.loading = true;
+        this._patientEditService
+            .update(this.patient)
+            .pipe()
+            .subscribe(
+                data => {
+                    this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/admin/patient/list';
+                    this.router.navigateByUrl(this.returnUrl);
+                },
+                error => {
+                    this.error = error;
+                    this.loading = false;
+                }
+            );
+    }
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next();
+        this._unsubscribeAll.complete();
+    }
 }
--- DCRM_UI/DCRMUI/src/app/main/clinic-admin/patient/patient-edit/patient-edit.service.ts
+++ DCRM_UI/DCRMUI/src/app/main/clinic-admin/patient/patient-edit/patient-edit.service.ts
@@ -1,20 +1,104 @@
-// patient.service.ts
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { environment } from 'environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../../../../auth/models';
 
 @Injectable()
+export class PatientEditService implements Resolve<any> {
+    apiData: any;
+    onPatientEditChanged: BehaviorSubject<any>;
+    id;
+    currentUser: any;
 
-export class PatientEditService {
-  private apiUrl = 'https://localhost:7069/api';
+    /**
+     * Constructor
+     *
+     * @param {HttpClient} _httpClient
+     */
+    constructor(private _httpClient: HttpClient) {
+        // Set the defaults
+        this.currentUser = <User>JSON.parse(localStorage.getItem('currentUser'));
+        this.onPatientEditChanged = new BehaviorSubject({});
+    }
 
-  constructor(private http: HttpClient) {}
+    /**
+     * Resolver
+     *
+     * @param {ActivatedRouteSnapshot} route
+     * @param {RouterStateSnapshot} state
+     * @returns {Observable<any> | Promise<any> | any}
+     * 
+     */
 
-  getPatient(id: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}/Patient/Get/${id}`);
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+        let currentId = Number(route.paramMap.get('id'));
+        return new Promise<void>((resolve, reject) => {
+          /* this.getPatientList() */
+            Promise.all([this.getApiData(currentId)]).then(() => {
+                resolve();
+            }, reject);
+        });
+    }
+
+    getPatientList(): Promise<any[]> {
+      const headers = new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${this.currentUser.jwtToken}`
+      });
+      const requestOptions = { headers: headers };
+      const url = `${environment.apiUrl}/Patient/List`; // Adjust the URL as needed
+      return new Promise((resolve, reject) => {
+          this._httpClient.get(url, requestOptions).subscribe((response: any) => {
+              // Handle the response as needed
+              resolve(response);
+          }, reject);
+      });
   }
+  
+  
+    getApiData(id: number): Promise<any[]> {
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.currentUser.jwtToken}`
+        });
+        const requestOptions = { headers: headers };
+        const url = `${environment.apiUrl}/Patient/Get/${id}`;
+        this.id = id;
+        return new Promise((resolve, reject) => {
+            this._httpClient.get(url, requestOptions).subscribe((response: any) => {
 
-  updatePatient(id: number, patientData: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/Patient/Update/${id}`, patientData);
-  }
+                this.apiData = response;
+                this.onPatientEditChanged.next(this.apiData);
+                resolve(this.apiData);
+            }, reject);
+        });
+    }
+    update(patient: any) {
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.currentUser.jwtToken}`
+        });
+        const requestOptions = { headers: headers };
+        debugger;
+        return this._httpClient.post<any>(`${environment.apiUrl}/Patient/Update`, patient, requestOptions);
+    }
+
+
 }
+
+
+// export class PatientEditService {
+//   private apiUrl = 'https://localhost:7069/api';
+
+//   constructor(private http: HttpClient) {}
+
+//   getPatient(id: number): Observable<any> {
+//     return this.http.get(`${this.apiUrl}/Patient/Get/${id}`);
+//   }
+
+//   updatePatient(id: number, patientData: any): Observable<any> {
+//     return this.http.put(`${this.apiUrl}/Patient/Update/${id}`, patientData);
+//   }
+// }
