3fb556c93eba4f073b37422d9dbf0ca72acdcb6e
ezsiddharth <145989806+ezsiddharth@users.noreply.github.com>
2023-10-05 11:40:47 +0530

    Patient Add Updated & Modified

--- DCRM_UI/DCRMUI/src/app/main/clinic-admin/patient/patient-add/patient-add.component.ts
+++ DCRM_UI/DCRMUI/src/app/main/clinic-admin/patient/patient-add/patient-add.component.ts
@@ -5,7 +5,7 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { PatientAddService } from 'app/main/clinic-admin/patient/patient-add/patient-add.service';
-import { PatientAddModel } from './patient-add.model';
+import { PatientAddModel } from '../patient-add.model';
 import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
@@ -17,7 +17,7 @@ import { Observable } from 'rxjs';
   encapsulation: ViewEncapsulation.None
 })
 export class PatientAddComponent implements OnInit, OnDestroy {
-  public addPatientForm: FormGroup;
+  public addPatientForm: UntypedFormGroup;
   public error: string = '';
   public insuranceLoanItems: any[] = [];
   public loading = false;
@@ -28,8 +28,8 @@ export class PatientAddComponent implements OnInit, OnDestroy {
 
   private _unsubscribeAll: Subject<any>;
   public patient: PatientAddModel = {
-    title: '',
-    photo: null,
+    title: 'a',
+    thumb: null,
     name: '',
     guardian: '',
     sex: '',
@@ -40,10 +40,10 @@ export class PatientAddComponent implements OnInit, OnDestroy {
     present_address: '',
     permanent_address: '',
     patientContacts: [{
-      phone1: null,
-      phone2: null,
-      phone3: null,
-      phone4: null,
+      phone1: 0,
+      phone2: 0,
+      phone3: 0,
+      phone4: 0,
       email: '',
       email2: '',
       address_R: '',
@@ -56,12 +56,12 @@ export class PatientAddComponent implements OnInit, OnDestroy {
       country_O: '',
       address_Other: '',
       city_Other: '',
-      zip_Other: null,
+      zip_Other: 0,
       country_Other: '',
       physician: '',
       reffered_By: '',
       doctor_Name: '',
-      phone: null,
+      phone: '',
       relationship_Type: '',
       history_Allergies: '',
       special_Notes: '',  
@@ -78,12 +78,15 @@ export class PatientAddComponent implements OnInit, OnDestroy {
       const reader = new FileReader();
       reader.onload = (e: any) => {
         this.base64Image = e.target.result;
+        
       };
       reader.onload = (e: any) => {
+        this.base64Image = e.target.result;
         this.imagePreviewUrl = e.target.result;
+        debugger;
       };
       reader.readAsDataURL(file);
-      reader.readAsDataURL(this.selectedImage);
+      //reader.readAsDataURL(this.selectedImage);
     }
   }
 
@@ -93,54 +96,20 @@ export class PatientAddComponent implements OnInit, OnDestroy {
 
   constructor(
     private router: Router,
-    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private httpClient: HttpClient,
     private http: HttpClient,
-    private patientAddService: PatientAddService, private _formBuilder: UntypedFormBuilder, private _route: ActivatedRoute, private _toastrService: ToastrService) {
+    private patientAddService: PatientAddService, 
+    private _formBuilder: UntypedFormBuilder, 
+    private _route: ActivatedRoute, 
+    private _toastrService: ToastrService) {
     this._unsubscribeAll = new Subject();
   }
 
   ngOnInit(): void {
-    this.addPatientForm = this.formBuilder.group({
-      title: ['select'],
-      name: ['', Validators.required],
-      guardian: [''],
-      sex: [''],
-      dob: [null],
-      age: [null],
-      weight: [null],
-      mobile: [null],
-      present_address: [''],
-      permanent_address: [''],
-      patientContacts: {
-        phone1: [null, Validators.required],
-        phone2: [null],
-        phone3: [null],
-        phone4: [null],
-        email: [''],
-        email2: [''],
-        address_R: [''],
-        city_R: [''],
-        zip_R: [''],
-        country_R: [''],
-        address_O: [''],
-        city_O: [''],
-        zip_O: [''],
-        country_O: [''],
-        address_Other: [''],
-        city_Other: [''],
-        zip_Other: [''],
-        country_Other: [''],
-        physician: [''],
-        referred_by: [''],
-        doctor_name: [''],
-        phone_doctor: [null],
-        relationship_type: [''],
-        history_allergies: [''],
-        special_Notes: [''],
-      }
-      
+    this.addPatientForm = this._formBuilder.group({
+        name: ['', Validators.required],
+        phone1: ['', Validators.required]
     });
   }
 
@@ -154,54 +123,27 @@ export class PatientAddComponent implements OnInit, OnDestroy {
   } 
 
   onSubmit() {
-    this.submitted = true;
-    debugger;
-    const patientData = {
-      name: this.addPatientForm.get('name').value,
-      patientContacts: [{
-        phone1: this.addPatientForm.get('patientContacts').value.phone1,
-        // phone1: 6755463,
-      }]
+    this.submitted = true; 
+    if (this.addPatientForm.invalid) {
+          return;
     }
-    
-    console.log(patientData);
-
-
-        if (this.addPatientForm.invalid) {
-            return;
-        }
- 
-        this.loading = true;
-        this.patientAddService
-          .update(patientData)
-          .pipe()
-          // .subscribe(
-          //     data => {
-          //         this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/admin/patient/list';
-          //         this.router.navigateByUrl(this.returnUrl);
-          //     },
-          //     error => {
-          //         this.error = error;
-          //         this.loading = false;
-          //     }
-          // );
-
-          this.patientAddService.update(patientData).subscribe(
-            (response) => {
-              // Handle the response here
-              console.log('Patient data updated:', response);
-              // Redirect or perform other actions as needed
+    var abc = this.patient.thumb;
+    // this.patient.thumb = this.base64Image.split(',')[0];
+    debugger;
+    this.loading = true;
+    this.patientAddService
+      .update(this.patient)
+      .pipe()
+      .subscribe(
+          data => {
               this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/admin/patient/list';
               this.router.navigateByUrl(this.returnUrl);
-            },
-            (error) => {
-              // Handle errors here
-              console.error('Error updating patient data:', error);
+          },
+          error => {
               this.error = error;
               this.loading = false;
-            }
-          ); 
-            
+          }
+      );  
   }
 
   items: any[] = [];
@@ -220,58 +162,6 @@ export class PatientAddComponent implements OnInit, OnDestroy {
     this.items.splice(index, 1);
   }
 
-  // saveData() {
-  //   const dataToSave = {
-  //       'id': 0,
-  //       'name': this.patient.name || '', 
-  //       'guardian': this.patient.guardian || '',
-  //       'sex': this.patient.sex || '',
-  //       'dob': this.patient.dob || '',
-  //       'age': this.patient.age || '',
-  //       'weight': this.patient.weight || '',
-  //       'mobile': this.patient.mobile || '',
-  //       'present_address': this.patient.present_address || '',
-  //       'permanent_address': this.patient.permanent_address || '',
-  //       'patientContacts': this.patient.patientContacts.map(contact => ({
-  //           'id': 0,
-  //           'patient_Id': 0,
-  //           'phone1': contact.phone1 || 0,
-  //           'Address_O': contact.address_O || '',
-  //           'Address_Other': contact.address_Other || '',
-  //           'Address_R': contact.address_R || '',
-  //           'City_O': contact.city_O || '',
-  //           'City_Other': contact.city_Other || '',
-  //           'City_R': contact.city_R || '',
-  //           'Country_O': contact.country_O || '',
-  //           'Country_Other': contact.country_Other || '',
-  //           'Country_R': contact.country_R || '',
-  //           'Doctor_Name': contact.doctor_Name || '',
-  //           'Email': contact.email || '',
-  //           'Email2': contact.email2 || '',
-  //           'Medical_History_Allergies': contact.history_Allergies || '',
-  //           'Phone': contact.phone || '',
-  //           'Physician': contact.physician || '',
-  //           'Reffered_By': contact.reffered_By || '',
-  //           'Relationship_Type': contact.relationship_Type || '',
-  //           'Special_Notes': contact.special_Notes || '',
-  //           'Zip_O': contact.zip_O || '',
-  //           'Zip_R': contact.zip_R || '',
-  //       }))
-  //   };
-
-  //   // Make an HTTP POST request to save the data
-  //   const apiUrl = '${environment.apiUrl}/Patient/Create'; 
-  //   this.patientAddService.savePatient(dataToSave).subscribe(
-  //       (response) => {
-  //           // Handle the response here
-  //           console.log('Data saved:', response);
-  //       },
-  //       (error) => {
-  //           // Handle errors here
-  //           console.error('Error:', error);
-  //       }
-  //   )
-  // }
   ngOnDestroy(): void {
     this._unsubscribeAll.next();
     this._unsubscribeAll.complete();
