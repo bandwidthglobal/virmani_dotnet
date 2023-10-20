3ca72a50d1c224e6740ce0480fee07d10f7004df
rajaram-rm <raja@eztechnologies.in>
2023-10-20 09:55:42 +0530

    code merged

--- DCRM_UI/DCRMUI/src/app/shared-common/shared-ui.module.ts
+++ DCRM_UI/DCRMUI/src/app/shared-common/shared-ui.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorMessagesPipe } from './pipes/error-message.pipe';
+import { UiTextInputComponent } from './ui-inputs/ui-text-input/ui-text-input.component';
+import { UiEmailInputComponent } from './ui-inputs/ui-email-input/ui-email-input.component';
+import { UiDateInputComponent } from './ui-inputs/ui-date-input/ui-date-input.component';
+import { UiSelectInputComponent } from './ui-inputs/ui-select-input/ui-select-input.component';
 
 const components = [
-  ErrorMessagesPipe
+  ErrorMessagesPipe,
+  UiTextInputComponent,
+  UiEmailInputComponent,
+  UiDateInputComponent,
+  UiSelectInputComponent
 ];
 
 
